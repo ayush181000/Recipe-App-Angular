2be67f0914ef4f309b0f5d558e928de7232888e2
Ayush Garg <ayush181000@gmail.com>
2022-12-05 19:49:23 +0530

    display details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://live.staticflickr.com/7423/28115954606_5a068f9d36_b.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://live.staticflickr.com/7423/28115954606_5a068f9d36_b.jpg'
+    ),
+  ];
 }
