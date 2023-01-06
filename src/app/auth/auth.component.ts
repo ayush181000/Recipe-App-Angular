b747a99927051ab4bdb24ce0fb0479d1318e1443
Ayush Garg <ayush181000@gmail.com>
2023-01-06 07:09:20 +0530

    auth implemented- login and signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,8 +10,45 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService, private router: Router) {
+    // this.authService.user.subscribe((user) => console.log(user));
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+      // login
+      authObs = this.authService.login(form.value.email, form.value.password);
+    } else {
+      // signup
+      authObs = this.authService.signup(form.value.email, form.value.password);
+    }
+
+    authObs.subscribe({
+      next: (resData) => {
+        this.router.navigate(['/recipes']);
+      },
+      error: (errorMessage) => {
+        this.error = errorMessage;
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+
+    form.reset();
   }
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,98 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { catchError, tap } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { User } from './user.model';
+
+export interface AuthResponseData {
+  kind: string;
+  email: string;
+  idToken: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  user = new Subject<User>();
+
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBO5J0WrQuECBiso-fg3P1QFazvgE93LU0',
+        { email, password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) =>
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          )
+        )
+      );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBO5J0WrQuECBiso-fg3P1QFazvgE93LU0',
+        { email, password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) =>
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          )
+        )
+      );
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    console.log(errorRes);
+    let errorMessage = 'An unkonwn error occurred!';
+    if (errorRes.error && errorRes.error.error) {
+      switch (errorRes.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'This email already exists';
+          break;
+        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+          errorMessage = 'Too may attempts. Try Later';
+          break;
+        case 'EMAIL_NOT_FOUND':
+          errorMessage = 'This email does not exists';
+          break;
+        case 'INVALID_PASSWORD':
+          errorMessage = 'Incorrect Password';
+          break;
+        case 'USER_DISABLED':
+          errorMessage =
+            'The user account has been disabled by an administrator.';
+          break;
+      }
+    }
+    return throwError(() => errorMessage);
+  }
+}
